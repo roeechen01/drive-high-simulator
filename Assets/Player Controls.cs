ef0fe7a20302ee4b6af174790d127988422296c4
Roee Chen <roeechen01@gmail.com>
2021-05-11 01:02:37 +0300

    initial commit

---
+++ Assets/Player Controls.cs
@@ -0,0 +1,306 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Player Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Player Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""9fb8c2e1-f959-4462-8679-596c8678d845"",
+            ""actions"": [
+                {
+                    ""name"": ""Gas"",
+                    ""type"": ""Button"",
+                    ""id"": ""f3bf0945-f4bf-43fe-9bc2-edeabfec96e2"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Reverse"",
+                    ""type"": ""Button"",
+                    ""id"": ""baf038b3-ffaf-4d11-b1c9-8866d024577c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""85131782-df9d-44bd-9e3e-d18f48328954"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""View"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""eaf15b21-7e1c-4767-99f9-64c7d6af133b"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""ResetCamera"",
+                    ""type"": ""Button"",
+                    ""id"": ""d128d0f4-45c4-49cc-a1a4-88b72e553a2a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""ReverseCamera"",
+                    ""type"": ""Button"",
+                    ""id"": ""41fcee48-6a60-4d5a-a396-ee895d617d88"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(pressPoint=0.1)""
+                },
+                {
+                    ""name"": ""Quit"",
+                    ""type"": ""Button"",
+                    ""id"": ""fd78a256-263b-4e26-ba1a-35a26b8a01b1"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(pressPoint=0.1)""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""29eb365e-5932-4842-8a2f-0a73c0e044be"",
+                    ""path"": ""<Gamepad>/rightTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Gas"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a921edb2-927e-4b72-b413-5f6cb83f880a"",
+                    ""path"": ""<Gamepad>/leftTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Reverse"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5b239516-e6d6-4c42-aa5c-ffb4e11a2ec9"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cb1a464d-c47b-4f53-a146-6a234441241b"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""View"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""269e204c-6e19-4590-9ebf-5e7065dbf092"",
+                    ""path"": ""<Gamepad>/buttonNorth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ResetCamera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5349f52b-4d16-4029-a94e-853752edfdd6"",
+                    ""path"": ""<Gamepad>/rightStickPress"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ReverseCamera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""84196395-d577-4c0f-9901-b27bebb6844f"",
+                    ""path"": ""<Gamepad>/select"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Quit"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Gas = m_Gameplay.FindAction("Gas", throwIfNotFound: true);
+        m_Gameplay_Reverse = m_Gameplay.FindAction("Reverse", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        m_Gameplay_View = m_Gameplay.FindAction("View", throwIfNotFound: true);
+        m_Gameplay_ResetCamera = m_Gameplay.FindAction("ResetCamera", throwIfNotFound: true);
+        m_Gameplay_ReverseCamera = m_Gameplay.FindAction("ReverseCamera", throwIfNotFound: true);
+        m_Gameplay_Quit = m_Gameplay.FindAction("Quit", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Gas;
+    private readonly InputAction m_Gameplay_Reverse;
+    private readonly InputAction m_Gameplay_Move;
+    private readonly InputAction m_Gameplay_View;
+    private readonly InputAction m_Gameplay_ResetCamera;
+    private readonly InputAction m_Gameplay_ReverseCamera;
+    private readonly InputAction m_Gameplay_Quit;
+    public struct GameplayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GameplayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Gas => m_Wrapper.m_Gameplay_Gas;
+        public InputAction @Reverse => m_Wrapper.m_Gameplay_Reverse;
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputAction @View => m_Wrapper.m_Gameplay_View;
+        public InputAction @ResetCamera => m_Wrapper.m_Gameplay_ResetCamera;
+        public InputAction @ReverseCamera => m_Wrapper.m_Gameplay_ReverseCamera;
+        public InputAction @Quit => m_Wrapper.m_Gameplay_Quit;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Gas.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnGas;
+                @Gas.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnGas;
+                @Gas.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnGas;
+                @Reverse.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnReverse;
+                @Reverse.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnReverse;
+                @Reverse.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnReverse;
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @View.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnView;
+                @View.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnView;
+                @View.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnView;
+                @ResetCamera.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnResetCamera;
+                @ResetCamera.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnResetCamera;
+                @ResetCamera.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnResetCamera;
+                @ReverseCamera.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnReverseCamera;
+                @ReverseCamera.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnReverseCamera;
+                @ReverseCamera.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnReverseCamera;
+                @Quit.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnQuit;
+                @Quit.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnQuit;
+                @Quit.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnQuit;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Gas.started += instance.OnGas;
+                @Gas.performed += instance.OnGas;
+                @Gas.canceled += instance.OnGas;
+                @Reverse.started += instance.OnReverse;
+                @Reverse.performed += instance.OnReverse;
+                @Reverse.canceled += instance.OnReverse;
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @View.started += instance.OnView;
+                @View.performed += instance.OnView;
+                @View.canceled += instance.OnView;
+                @ResetCamera.started += instance.OnResetCamera;
+                @ResetCamera.performed += instance.OnResetCamera;
+                @ResetCamera.canceled += instance.OnResetCamera;
+                @ReverseCamera.started += instance.OnReverseCamera;
+                @ReverseCamera.performed += instance.OnReverseCamera;
+                @ReverseCamera.canceled += instance.OnReverseCamera;
+                @Quit.started += instance.OnQuit;
+                @Quit.performed += instance.OnQuit;
+                @Quit.canceled += instance.OnQuit;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnGas(InputAction.CallbackContext context);
+        void OnReverse(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
+        void OnView(InputAction.CallbackContext context);
+        void OnResetCamera(InputAction.CallbackContext context);
+        void OnReverseCamera(InputAction.CallbackContext context);
+        void OnQuit(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerCar.cs
@@ -0,0 +1,251 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCar : MonoBehaviour
+{
+    Camera myCamera;
+    Rigidbody myRigidbody;
+    float speed = 10000f;
+    float currentSpeed = 0f;
+    float rotationSpeed = 150f;
+    float cameraSpeed = 400f;
+    PlayerControls controls;
+    Vector2 direction;
+    Vector2 view;
+    float gasAmount;
+    float reverseAmount;
+    bool gas = true;
+    bool reverse = true;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Gameplay.Gas.performed += ctx => gasAmount = ctx.ReadValue<float>();
+        //controls.Gameplay.Gas.performed += ctx => Gas();
+        controls.Gameplay.Gas.canceled += ctx => Stop();
+
+        controls.Gameplay.Reverse.performed += ctx => reverseAmount = ctx.ReadValue<float>();
+        controls.Gameplay.Reverse.canceled += ctx => Stop();
+
+        controls.Gameplay.Move.performed += ctx => direction = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += ctx => direction = Vector2.zero;
+
+        controls.Gameplay.View.performed += ctx => view = ctx.ReadValue<Vector2>();
+        controls.Gameplay.View.canceled += ctx => view = Vector2.zero;
+
+        controls.Gameplay.ResetCamera.performed += ctx => ResetCamera();
+
+        controls.Gameplay.ReverseCamera.performed += ctx => ReverseCamera();
+        controls.Gameplay.ReverseCamera.canceled += ctx => ReverseCamera();
+
+        controls.Gameplay.Quit.performed += ctx => Application.Quit();
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (Input.GetKeyDown(KeyCode.Escape))
+            //Application.Quit();
+        myRigidbody = GetComponent<Rigidbody>();
+        myCamera = FindObjectOfType<Camera>();
+        myCamera.transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y + 0.8f, transform.position.z - 0.6f);
+        //myCamera.transform.rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+
+    void Slow()
+    {
+
+    }
+
+    void Update()
+    {
+        gas = gasAmount > 0.15f;
+        reverse = reverseAmount > 0.15f;
+
+        if (myRigidbody.velocity != Vector3.zero)
+        {
+            
+            if (gas)
+            {
+                transform.Rotate(Vector3.up * direction.x * rotationSpeed * Time.deltaTime);
+                myRigidbody.velocity = transform.forward * (gasAmount) * speed * Time.deltaTime;
+            }
+            else
+            {
+               
+                if (!reverse)
+                {
+                    //myRigidbody.velocity *= 0.5f;
+                    //myRigidbody.velocity = transform.forward * Time.deltaTime * speed;
+                    //myRigidbody.velocity += -transform.forward;
+                    //if (myRigidbody.velocity.magnitude < 1)
+                    
+                    myRigidbody.velocity = Vector3.zero;
+                }
+                else
+                {
+                    transform.Rotate(Vector3.up * -direction.x * rotationSpeed * Time.deltaTime);
+                    myRigidbody.velocity = transform.forward * (reverseAmount) * -speed * Time.deltaTime;
+                }
+
+            }
+        }
+        else
+        {
+            if (gas)
+            {
+                //transform.Rotate(Vector3.up * direction.x * rotationSpeed * Time.deltaTime);
+                myRigidbody.velocity += transform.forward * (gasAmount) * speed * Time.deltaTime;
+            }
+            if (reverse)
+            {
+                myRigidbody.velocity += transform.forward * (reverseAmount) * -speed * Time.deltaTime;
+            }
+        }
+
+            /*if (gasAmount > 0)
+        {
+            transform.Rotate(Vector3.up * direction.x * rotationSpeed * Time.deltaTime);
+            myRigidbody.velocity += transform.forward * (gasAmount * 1f) * speed * Time.deltaTime;
+
+        }
+        else if (breaksAmount == 0)
+        {
+            //print("Entered: " + myRigidbody.velocity);
+            if (myRigidbody.velocity != Vector3.zero)
+            {
+                transform.Rotate(Vector3.up * direction.x * rotationSpeed * Time.deltaTime);
+                //myRigidbody.velocity -= transform.forward * 1000f * Time.deltaTime;
+                myRigidbody.velocity /= 2f * Time.deltaTime;
+                //print("Entered: " + myRigidbody.velocity);
+            }
+        }*/
+
+        if (myRigidbody.velocity != Vector3.zero)
+        {
+            //if (gas)
+            //{
+                //transform.Rotate(Vector3.up * direction.x * rotationSpeed * Time.deltaTime);
+                //myRigidbody.velocity = transform.forward * Time.deltaTime; 
+            //}
+            //else
+            //{
+                //transform.Rotate(Vector3.up * -direction.x * rotationSpeed * Time.deltaTime);
+                //myRigidbody.velocity = transform.forward * -speed * Time.deltaTime;
+            //}
+        }
+        
+        if(view != Vector2.zero)
+        {
+            myCamera.transform.Rotate(new Vector3(0, view.x, 0) * cameraSpeed * Time.deltaTime);//for one direction viewing
+            //myCamera.transform.Rotate(new Vector3(view.y, view.x, 0) * cameraSpeed * Time.deltaTime);//for two directions viewing
+        }
+
+        
+
+
+
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+    void Gas()
+    {
+        //gas = true;
+        myRigidbody.velocity = transform.forward * speed * Time.deltaTime;
+        
+    }
+
+    void ResetCamera()
+    {
+        myCamera.transform.rotation = transform.rotation;
+    }
+
+    void ReverseCamera()
+    {
+        myCamera.transform.Rotate(new Vector3(0, 1, 0), 180f);
+        print("called");
+    }
+
+    /*void Repeat()
+    {
+        if (myRigidbody.velocity != Vector3.zero)
+        {
+            if (gas)
+            {
+                transform.Rotate(Vector3.up * direction2.x * rotationSpeed * Time.deltaTime);
+                myRigidbody.velocity = transform.forward * speed * Time.deltaTime;
+            }
+            else
+            {
+                transform.Rotate(Vector3.up * -direction2.x * rotationSpeed * Time.deltaTime);
+                myRigidbody.velocity = transform.forward * -speed * Time.deltaTime;
+            }
+        }
+        else CancelInvoke("Repeat");
+        print(direction2);
+            
+    }*/
+
+    void Reverse()
+    {
+        //gas = false;
+        myRigidbody.velocity = transform.forward * -speed * Time.deltaTime;
+    }
+
+    void Stop()
+    {
+        myRigidbody.velocity = Vector3.zero;
+    }
+
+    /*void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            myRigidbody.velocity = transform.forward * speed * Time.deltaTime;
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            myRigidbody.velocity = Vector3.zero;
+        }
+       
+        if (Input.GetKey(KeyCode.S))
+        {
+            myRigidbody.velocity = transform.forward * -speed * Time.deltaTime;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            myRigidbody.velocity = Vector3.zero;
+        }
+    }*/
+
+    void ControlRotation()
+    {
+        if (Input.GetKey(KeyCode.A) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+        {
+            transform.Rotate(Vector3.down * rotationSpeed);
+        }
+        
+        if (Input.GetKey(KeyCode.D) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+        {
+            transform.Rotate(Vector3.up * rotationSpeed);
+        }
+        
+
+    }
+}
