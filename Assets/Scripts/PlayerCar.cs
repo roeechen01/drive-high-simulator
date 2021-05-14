d67dd743df4df1906f18b49c3290711b32621770
Roee Chen <roeechen01@gmail.com>
2021-05-15 01:40:52 +0300

    Now camera viewing is more responsive when it reaches a limit & removed no longer necessary code

--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -14,13 +14,12 @@ public class PlayerCar : MonoBehaviour
     float currentSound = 0f;
     float currentSpeed = 0f;
     float rotationSpeed = 125f;
-    float cameraSpeed = 150f;
+    float cameraSpeed = 200f;
     PlayerControls controls;
     Vector2 direction;
     Vector2 view;
     float gasAmount;
     float reverseAmount;
-    Vector2 last = Vector2.zero;
     public bool build = false;
     public float buildDifference = 5.5f;
     Radio radio;
@@ -135,18 +134,14 @@ public class PlayerCar : MonoBehaviour
         myCamera.transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y + 0.8f, transform.position.z - 0.6f);
         InvokeRepeating("CheckPedals", 0.1f, 0.1f);
 
-        //InvokeRepeating("ChangeCameraDirection", 0f, 0.01f);
-
         if (build)
             speedAddition /= buildDifference;
         else buildDifference = 1;
     }
 
-    float multiplier = 75f;
-    //int counter;
     void ChangeCameraDirection()
     {
-        float stickError = 0.1f;
+        float stickError = 0.1f, back = 0.075f;
         if (view.x > stickError || -view.x > stickError || view.y > stickError || -view.y > stickError)
         {
             if (!r3)
@@ -158,10 +153,10 @@ public class PlayerCar : MonoBehaviour
                 else
                 {
                     if (myCamera.transform.localRotation.eulerAngles.y <= 275)
-                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y - 0.01f, 0f);
+                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y - back, 0f);
 
                     if (myCamera.transform.localRotation.eulerAngles.y >= 85)
-                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y + 0.01f, 0f);
+                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y + back, 0f);
                 }
                 if (myCamera.transform.localRotation.eulerAngles.x < 75 || myCamera.transform.localRotation.eulerAngles.x > 285)
                 {
@@ -170,10 +165,10 @@ public class PlayerCar : MonoBehaviour
                 else
                 {
                     if (myCamera.transform.localRotation.eulerAngles.x <= 275)
-                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x - 0.01f, myCamera.transform.localRotation.eulerAngles.y, 0f);
+                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x - back, myCamera.transform.localRotation.eulerAngles.y, 0f);
 
                     if (myCamera.transform.localRotation.eulerAngles.x >= 85)
-                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x + 0.01f, myCamera.transform.localRotation.eulerAngles.y, 0f);
+                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x + back, myCamera.transform.localRotation.eulerAngles.y, 0f);
                 }
             }
             else
@@ -185,10 +180,10 @@ public class PlayerCar : MonoBehaviour
                 else
                 {
                     if (myCamera.transform.localRotation.eulerAngles.y >= 115)
-                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y - 0.01f, 0f);
+                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y - back, 0f);
 
                     if (myCamera.transform.localRotation.eulerAngles.y <= 245)
-                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y + 0.01f, 0f);
+                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y + back, 0f);
                 }
 
                 if (myCamera.transform.localRotation.eulerAngles.x < 75 || myCamera.transform.localRotation.eulerAngles.x > 295)
@@ -198,52 +193,16 @@ public class PlayerCar : MonoBehaviour
                 else
                 {
                     if (myCamera.transform.localRotation.eulerAngles.x >= 285)
-                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x + 0.01f, myCamera.transform.localRotation.eulerAngles.y, 0f);
+                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x + back, myCamera.transform.localRotation.eulerAngles.y, 0f);
 
                     if (myCamera.transform.localRotation.eulerAngles.x <= 85)
-                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x - 0.01f, myCamera.transform.localRotation.eulerAngles.y, 0f);
+                        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x - back, myCamera.transform.localRotation.eulerAngles.y, 0f);
                 }
             }
             myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.eulerAngles.x, myCamera.transform.localRotation.eulerAngles.y, 0f);
         }
           
     }
-
-    float div = 50f;
-    Quaternion step;
-    void Animate()
-    {
-        Quaternion rotationTo = StickToRotation(view);
-        Quaternion rot = Quaternion.FromToRotation(new Vector3(myCamera.transform.localRotation.x, myCamera.transform.localRotation.y, myCamera.transform.localRotation.z) , new Vector3(rotationTo.x, rotationTo.y, rotationTo.z));
-        //myCamera.transform.rotation = rotationTo;
-        step = rot ;
-        step.x = rot.x / div;
-        step.y = rot.y / div;
-        InvokeRepeating("Step", 0f, 0.1f);
-
-    }
-
-    void Step()
-    {
-        print(step.x + ", " + step.y);
-        myCamera.transform.localRotation = Quaternion.Euler(myCamera.transform.localRotation.x + step.x, myCamera.transform.localRotation.y + step.y, 0f);
-        //counter--;
-        //if (counter <= 0)
-            //CancelInvoke("Step");
-    }
-
-    Quaternion StickToRotation(Vector2 stick)
-    {
-        Quaternion rotation = Quaternion.identity;
-        rotation.x = -stick.y * multiplier;
-        rotation.y = stick.x * multiplier;
-        return rotation;
-    }
-
-    /*Vector2 RotationToStick(Vector2 rotation)
-    {
-        return new Vector2(rotation.y / multiplier , -rotation.x / multiplier );
-    }*/
 
     private void OnEnable()
     {
